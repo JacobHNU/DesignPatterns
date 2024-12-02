f15a8518088b289e8e4467095e3b8cad5fa87119
lp <lp@qq.com>
2024-12-02 23:28:30 +0800

    IPS

---
+++ 接口隔离原则（Interface Segregation Principle, IPS）/ConsoleApp1/Printer1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.JScript;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Printer1
+    {
+        private Object document;
+
+        public Printer1(Object document)
+        {
+            this.document = document;
+        }
+
+        public void print()
+        {
+            if (document is Document) {
+                ((Document)document).PrintContent();
+            } else if (document is ImageDocument) {
+                ((ImageDocument)document).PrintContent();
+            }
+        }
+    }
+    //这种设计显然不理想，因为每次添加新的文档类型都需要修改 Printer 类，增加了维护的复杂性。
+}
